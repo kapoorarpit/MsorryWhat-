6d1332c03def90255bcbaac612114447f0ab4a00
kapoorarpit <kapoorarpit2000@gmail.com>
2021-09-22 01:38:12 +0530

    _

--- index.js
+++ index.js
@@ -12,30 +12,92 @@ recognition.onresult = function(event) {
     transcript = event.results[current][0].transcript;
 
     content+=transcript;
-    document.getElementById("instructions").textContent=content
     console.log(content)
 };
 
 
-recognition.onend = function() {
-    console.log('Speech recognition service disconnected');
-    recognition.start();
-}
-
 recognition.continuous = true;
-recognition.start();
 
 var username=""
 var contact=""
 var item=""
 document.getElementById("notify").innerText="Please Submit details to get notification on your mobile number"
 
+var submitted=false;
 var submit = document.getElementById("submit").addEventListener("click",function(event){
     username=document.getElementById("user_name").value.trim()
     contact=document.getElementById("phone_number").value.trim()
     item=document.getElementById("item").value.trim()
+    if(!validate(username,contact,item)){return}
+    if(!phonenumber(contact))
+    {return}
     window.console.log(username)
     console.log(contact)
     console.log(item)
-    document.getElementById("notify").innerText="Hi "+ username +"! You'll get a text message on "+ contact +
-                                                " whenever '" + item +"' will be called in the meeting "})
+    document.getElementById("notify").innerHTML="Hi "+ username +"! You'll get a text message on "+ contact +
+                                                " whenever '" + item.bold() +"' will be called in the meeting "
+    submitted=true;
+})
+
+
+/*
+document.getElementById('listen').addEventListener('click',function(event){
+    console.log("yes it worked")
+})*/
+
+
+var listening_desired=true
+
+var listen_button=document.getElementById('listen')
+listen_button.addEventListener('click',function(event){
+    if(submitted==false){
+        alert("Please submit required details first")
+        return 
+    }
+    if(listen_button.innerText=="Start listening"){
+    listen_button.innerText="Stop listening"
+    recognition.start();
+    listening_desired=true;
+    }
+    else{
+    listen_button.innerText="Start listening"
+    listening_desired=false;
+    recognition.stop()
+    }
+})
+
+recognition.onend = function() {
+    console.log('Speech recognition service disconnected');
+    if(listening_desired==true)
+    recognition.start();
+}
+
+function phonenumber(inputtxt)
+{
+if (/^\d{10}$/.test(inputtxt)) {
+    // value is ok, use it
+    return true
+} else {
+    alert("Please enter valid cell number")
+    return false
+}
+}
+
+function validate(username, contact, item){
+    if(!username)
+    {
+    alert("username is required")
+    return false
+    }
+    if(!contact)
+    {
+    alert("cell number is required")
+    return false
+    }
+    if(!item)
+    {
+    alert("target name is required")
+    return false
+    }
+    return true
+}
