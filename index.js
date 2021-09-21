281fa7bcfcda4ecf45a08598a28955a1e705fa07
kapoorarpit <kapoorarpit2000@gmail.com>
2021-09-21 21:27:54 +0530

    _

--- index.js
+++ index.js
@@ -24,3 +24,18 @@ recognition.onend = function() {
 
 recognition.continuous = true;
 recognition.start();
+
+var username=""
+var contact=""
+var item=""
+var notify="Please Submit details to get notification on your mobile number"
+document.getElementById("notify").innerText=notify
+
+var submit = document.getElementById("submit").addEventListener("click",function(event){
+    username=document.getElementById("user_name").innerText
+    contact=document.getElementById("phone_number").textContent
+    item=document.getElementById("item").textContent
+    window.console.log(username)
+    console.log(contact)
+    console.log(item)
+})
