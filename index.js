3a0ffd862f0d3ecfa9f2a62502922451e3f9c5a3
kapoorarpit <kapoorarpit2000@gmail.com>
2021-10-29 16:41:35 +0530

    _

--- index.js
+++ index.js
@@ -114,6 +114,7 @@ req.headers({
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
   "Access-Control-Allow-Credentials": true,
+  "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
 });
 
 
